8d1c6e9b51ba938f44303b3c342a09a6ba948cf0
echoyan <echo_yan0919@163.com>
2026-09-24 20:40:31 +0800

    init

---
+++ src/main/java/com/echoyan/a2ahub/A2AHubApplication.java
@@ -0,0 +1,13 @@
+package com.echoyan.a2ahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2AHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2AHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/echoyan/a2ahub/A2AHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echoyan.a2ahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2AHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
